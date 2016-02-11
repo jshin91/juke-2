fece4cdf336b246297d14e558db12086338aa194
Terry Horowitz <terryhorowitz@Terrys-MacBook-Air.local>
2016-02-11 16:05:29 -0500

    commit

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -3,26 +3,20 @@
 juke.factory('PlayerFactory', function($log, $http, $rootScope){
   var songPlaying = false;
   var currentSong = null;
-  var songs, songList;
-  
-  $http.get('/api/albums')
-  .then(function(albums){
-    console.log(albums)
-    return albums.data[1];
-  })
-  .then(function(album){
-    songList = album.songs
-  }).catch($log.error);
+  var songs;
+  var songList = [];
+//  var progress = 0;
   
   var audio = document.createElement('audio');
-  // non-UI logic in here
+  
   function pause() {
     audio.pause();
     songPlaying = false;
   }
   
-  function start(song, songList) {
-  	pause();
+  function start(song, albumSongs) {
+  	player.pause();
+    songList = albumSongs;
     currentSong = song;
   	audio.src = currentSong.audioUrl
     audio.load();
@@ -44,16 +38,77 @@ juke.factory('PlayerFactory', function($log, $http, $rootScope){
   }
   
   function next () {
-    
+    var nextSong;
+    songList.forEach(function(song, index) {
+      if(song.audioUrl === currentSong.audioUrl) {
+        if(index == songList.length-1) {
+          nextSong = songList[0];
+        } else {
+        nextSong = songList[index + 1];
+        }
+      }
+    })
+    player.start(nextSong, songList);
   }
   
-  return {
+  function previous () {
+    var prevSong;
+    songList.forEach(function(song, index) {
+      if(song.audioUrl === currentSong.audioUrl) {
+        if(index == 0) {
+          prevSong = songList[songList.length-1];
+        } else {
+        prevSong = songList[index - 1];
+        }
+      }
+    })
+    player.start(prevSong, songList);
+  }  
+  
+  function getProgress() {
+    console.log(currentSong)
+    console.log('audio', audio)
+    var progress = 0;
+    if(songPlaying) {
+      progress = 100 * audio.currentTime / audio.duration;
+    }
+      console.log(audio.duration)
+      console.log('currtime', audio.currentTime)
+    return progress;
+  }
+  
+    
+    
+//  audio.addEventListener('ended', $scope.next);
+//  audio.addEventListener('timeupdate', function () {
+//    progress = 100 * audio.currentTime / audio.duration;
+//    console.log(audio.duration)
+//  });
+//    $rootScope.$digest(); // no Angular-aware code is doing this for us here
+//    console.log(audio)
+//    progress = 100 * audio.currentTime / audio.duration;
+  
+  
+  var player = { 
   	start: start,
     pause: pause,
     resume: resume,
     isPlaying: isPlaying,
     getCurrentSong: getCurrentSong,
-    next: next
+    next: next,
+    previous: previous,
+    getProgress: getProgress
   };
+  return player;
 });
+
+
+
+
+
+
+
+
+
+
 
