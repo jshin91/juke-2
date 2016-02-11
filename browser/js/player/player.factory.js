44ba4b20752b807c7518f988a5028d6e7d639ed0
Terry Horowitz <terryhorowitz@Terrys-MacBook-Air.local>
2016-02-11 14:35:12 -0500

    added start, resume, and isPlaying to player factory

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -1,18 +1,59 @@
 'use strict';
 
-juke.factory('PlayerFactory', function(){
-
+juke.factory('PlayerFactory', function($log, $http, $rootScope){
+  var songPlaying = false;
+  var currentSong = null;
+  var songs, songList;
+  
+  $http.get('/api/albums')
+  .then(function(albums){
+    console.log(albums)
+    return albums.data[1];
+  })
+  .then(function(album){
+    songList = album.songs
+  }).catch($log.error);
+  
   var audio = document.createElement('audio');
   // non-UI logic in here
-
-  function start(song) {
-  	audio.pause();
-  	audio.src = song.audioUrl
+  function pause() {
+    audio.pause();
+    songPlaying = false;
+  }
+  
+  function start(song, songList) {
+  	pause();
+    currentSong = song;
+  	audio.src = currentSong.audioUrl
     audio.load();
     audio.play();
+    songPlaying = true;
   }
+  
+  function resume() {
+    audio.play();
+    songPlaying = true;
+  }
+  
+  function isPlaying () {
+    return songPlaying;
+  }
+  
+  function getCurrentSong () {
+    return currentSong;
+  }
+  
+  function next () {
+    
+  }
+  
   return {
-  	start: start
+  	start: start,
+    pause: pause,
+    resume: resume,
+    isPlaying: isPlaying,
+    getCurrentSong: getCurrentSong,
+    next: next
   };
 });
 
